759013c81df5fc1496f35dfb797d361e892f7aec
kaitoyokohamaa <s1A121700885@toyo.jp>
2022-01-30 12:30:16 +0900

    make clip button function

--- screens/ArticleScreen.tsx
+++ screens/ArticleScreen.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
-import {StyleSheet, SafeAreaView, Text} from 'react-native';
-import {WebView} from 'react-native-webview';
+import React from "react";
+import { StyleSheet, SafeAreaView, Text, TouchableOpacity } from "react-native";
+import { WebView } from "react-native-webview";
+import { useDispatch, useSelector } from "react-redux";
+import { addClip, deleteClip } from "../store/actions/user";
+import { ClipButton } from "../components/ClipButton";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 });
 
-export default function ArticleScreen({route}) {
-  const {article} = route.params;
-
+export default function ArticleScreen({ route }) {
+  const { article } = route.params;
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+  const { clips } = user;
+  const isClipped = () => {
+    return clips.some((clip) => clip.url === article.url);
+  };
+  const toggleClip = () => {
+    if (isClipped()) {
+      dispatch(deleteClip({ clip: article }));
+    } else {
+      dispatch(addClip({ clip: article }));
+    }
+  };
   return (
     <SafeAreaView style={styles.container}>
-      <WebView source={{uri: article.url}} />
+      <ClipButton onPress={toggleClip} enabled={isClipped()} />
+
+      <WebView source={{ uri: article.url }} />
     </SafeAreaView>
   );
 }
