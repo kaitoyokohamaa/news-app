81a9e8bebca2ca1c2741e846e041df99b1282886
s1a121700885@toyo.jp <s1A121700885@toyo.jp>
2022-04-02 09:06:20 +0900

    git pull

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import AppNavigator from './navigation/AppNavigator';
+import {Provider} from 'react-redux';
+import store from './store';
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
 }
---
+++ store/index.tsx
@@ -0,0 +1,9 @@
+import {createStore, combineReducers} from 'redux';
+import userReducer from './reducers/user';
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const store = createStore(rootReducer);
+
+export default store;
